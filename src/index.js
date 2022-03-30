ec1575e7a1dbcba9b92aa6bf68468deeb00584e3
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-03-29 23:54:54 -0300

    feat: add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 const sum = require('./sum');
 
+var square = function (n) {
+    return n * n;
+}
+
+function fatorial(n){
+    if ((n == 0) || (n == 1))
+      return 1;
+    else
+      return (n * fatorial(n - 1));
+}
+
 console.log(sum(2, 2))
