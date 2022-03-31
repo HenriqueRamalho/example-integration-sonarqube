4ba5ab6177b4f976410c3bf4de4ec8041a6a0060
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-03-30 22:53:01 -0300

    refactor: organize math operations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const sum = require('./sum');
-
-var square = function (n) {
-    return n * n;
-}
-
-function fatorial(n){
-    if ((n == 0) || (n == 1))
-      return 1;
-    else
-      return (n * fatorial(n - 1));
-}
+const {sum, divide, multiply} = require('./operations');
 
 console.log(sum(2, 2))
+console.log(multiply(2, 3))
+console.log(divide(10, 2))
